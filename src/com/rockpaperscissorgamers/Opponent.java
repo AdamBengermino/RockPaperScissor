af5299106f920ec44906accbb2fbd1d5e8140fb7
Adam1313 <a3hm@comcast.net>
2016-08-12 16:26:47 -0400

    August122016 First Commit

---
+++ src/com/rockpaperscissorgamers/Opponent.java
@@ -0,0 +1,34 @@
+package com.rockpaperscissorgamers;
+
+public class Opponent extends Player
+{ 
+	protected boolean randomThrow = false; 
+	
+	public Opponent(String name, boolean randomThrow)
+	{
+		super(name);
+		this.randomThrow = randomThrow;
+	}
+	
+	public Opponent()
+	{
+	}
+
+	public boolean isRandomThrow()
+	{
+		return randomThrow;
+	}
+
+	public void setRandomThrow(boolean randomThrow)
+	{
+		this.randomThrow = randomThrow;
+	}
+
+	@Override
+	public void takeATurn()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
